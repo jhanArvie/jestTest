8b786f8d52370d9ed60e6c1639b6edd4ec5bd8ae
gaspar619 <jhanarvie@gmail.com>
2023-02-02 13:10:49 +0100

    Jest test

--- addFive.test.js
+++ addFive.test.js
@@ -1,4 +1,4 @@
-const { default: test } = require("node:test");//this appear after writing test() line 5
+const { default: TestRunner } = require("jest-runner");//this appear after writing test() line 5
 const addFive = require (`./addFive`);
 // the reason for the forward slash / is to indicate that the function is on the same folder as the test file
 test('returns the number plus 5', () =>{
